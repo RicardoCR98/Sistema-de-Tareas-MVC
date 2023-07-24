d0b6d5ee99181c474a8ba022bb084a60c8cab3f1
RicardoCR98 <gary.campana@epn.edu.ec>
2023-07-24 14:15:47 -0500

    Actualizacion 2

--- Lab03_SistemaDeTareas/src/main/java/controlador/CerrarSesionController.java
+++ Lab03_SistemaDeTareas/src/main/java/controlador/CerrarSesionController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package controlador;
 
 import java.io.IOException;
@@ -11,39 +10,24 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/CerrarSesionController")
 public class CerrarSesionController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+       
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate(); // Invalida la sesión actual y elimina todos los atributos de sesión
-        }
-        response.sendRedirect("index.html");
+    public CerrarSesionController() {
+        super();
     }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 HttpSession session = request.getSession(false);
+	        if (session != null) {
+	            session.invalidate(); // Invalida la sesión actual y elimina todos los atributos de sesión
+	        }
+	        response.sendRedirect("index.html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
 }
-=======
-package controlador;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-@WebServlet("/CerrarSesionController")
-public class CerrarSesionController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        HttpSession session = request.getSession(false);
-        if (session != null) {
-            session.invalidate(); // Invalida la sesión actual y elimina todos los atributos de sesión
-        }
-        response.sendRedirect("index.html");
-    }
-}
->>>>>>> branch 'main' of https://github.com/RicardoCR98/Sistema-de-Tareas-MVC.git
